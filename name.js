15f7f443d8a490ac9efedba3818c58d46152887f
salujan123 <161106744+salujan123@users.noreply.github.com>
2024-03-04 13:26:48 +0500

    Add files via upload

---
+++ name.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Salman', 'Amjad', 'Rehan', 'Hunaid',];
+const message = 'welcome to typescript class: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ name.ts
@@ -0,0 +1,8 @@
+
+
+let names :string[] = ['Salman', 'Amjad', 'Rehan', 'Hunaid',] ;
+
+const message :string = 'welcome to typescript class: '
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]) ;
+}
